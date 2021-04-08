6b4d45b01aea249d94380aabf48c941b562f2427
Paulo Ricardo <pauloricardoh@hotmail.com>
2021-04-07 22:56:02 -0300

    Aula 1 e 2

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+
+let plugins =[];
+
+if(process.env.NODE_ENV == 'production'){
+    plugins.push(new babiliPlugin());
+}
 
 module.exports = {
     entry: './app-src/app.js',
@@ -6,7 +13,8 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module : {
+    module : 
+    {
         rules : [
             {
                 test : /\.js$/,
@@ -16,5 +24,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins : plugins
 }     
